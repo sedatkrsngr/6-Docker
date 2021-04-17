47e7c49036b774cd9b103229db20d863e007568a
sedat karasungur <limitless_fearless@hotmail.com>
2021-04-17 17:44:28 +0300

    Console Docker oluşturuldu

---
+++ ConsoleDocker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleDocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
