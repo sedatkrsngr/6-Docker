1f99b0b8d1ebb05223b439d1de039be636a23f89
sedat karasungur <limitless_fearless@hotmail.com>
2021-04-24 23:59:32 +0300

    4-DockerVolume Bitti

--- MVCDockerVolume/Controllers/HomeController.cs
+++ MVCDockerVolume/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using MVCDockerVolume.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,14 +15,75 @@ namespace MVCDockerVolume.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IFileProvider _fileProvider;//startupta resim dosyalarını çekmek için oluşturduğum servisi alırım
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IFileProvider fileProvider)
         {
+            _fileProvider = fileProvider;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
+            //Docker Volume->kaydedilecek datalar farklı conteinerlarda alınan dataların ortak bir yerde kaydedilmesini sağlamak ve conteiner silinse dahi kaybolmamasını sağlamk için kullanılır. 3 Method vardır. Normalde ayağa kalkan conteinerlarda eklenen datalar sadece o conteiner için varolur.Bunun önünce geçmek için->
+            // 1-bind mount: İşletim sistemi üzerinde bir klasör açar ve tüm conteinerlar oraya kayıt yapar
+            // 2-volume: Docker üzerinde bir alan oluşturur ve tüm veriler buraya kaydolur. Bind amounttan artısı uzak sunucuya da kayıt yapabiliriz
+            // 3-Memory: İşletim sisteminin hafızasında dataları kaydeder.  Biz 1. ve 2. maddeleri bu eğitimde kullanıcaz
+
+
+            //1 Bind Amount
+            //Masaüstünde images diye bir klasör açtık
+            //docker run -d -p 5001:4500 --name mycon2 --mount type=bind,source="C:\Users\sedat.karasungur\Desktop\images",target="/app/wwwroot/images" 9a2
+            //kodun notları -d continer oluşurken ekranda göstermesin diye konuldu.  Verdiğimiz adres masaüstünde resimlerin kaydedileceği klasör. Target ise runtime image içerisinde publish çıkılmış dosyadaki images adresi 9a2 ise kullandığımız image id si
+            //Bind amount daha önce publish dosyasındaki images klasöründe resim varsa dahi ona bakmaz işletim sistemindeki klasöre bakar
+
+            //2 Volume
+            //docker volume create images  // docker üzerinde images adında volume oluşturduk
+            //docker run -d -p 5001:4500 --name mycon2 -v images:/app/wwwroot/images 9a2
+            //eğer silmek istersek te docker volume rm images  //images adındaki volume silinir
+            //kodun notları -v images -> volume images
+            //Bind amounttan farklı olarak images klasörü publish içindeki dataları da içerisine kopyalar. Böylece kayıp yaşanmaz
+
+
+            return View();
+        }
+
+        public IActionResult ImageShow()
+        {
+            var images = _fileProvider.GetDirectoryContents("wwwroot/images").ToList().Select(x=>x.Name);
+
+            return View(images);
+        }
+
+        [HttpPost]
+        public IActionResult ImageShow(string name)//silme işlemi için kullandık
+        {
+            var file = _fileProvider.GetDirectoryContents("wwwroot/images").ToList().First(x => x.Name == name);
+
+            System.IO.File.Delete(file.PhysicalPath);
+            return RedirectToAction("ImageShow");
+        }
+
+        public IActionResult ImageSave()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ImageSave(IFormFile imageFile)
+        {
+            if (imageFile != null && imageFile.Length>0)
+            {
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(imageFile.FileName); //resim1.jpg-> 6sad46-6asd4566-asdasd4.jpg gibi olacak
+
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", fileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await imageFile.CopyToAsync(stream);
+                }
+            
+            }
             return View();
         }
 
--- MVCDockerVolume/Startup.cs
+++ MVCDockerVolume/Startup.cs
@@ -3,9 +3,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -20,13 +22,16 @@ namespace MVCDockerVolume
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            IFileProvider fileProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory());//projemizdeki file datalarý çekebilmek için ekledik
+
+            services.AddSingleton<IFileProvider>(fileProvider);//servis olarak çalýþmasýný saðladýk program açýlýnca bir kez çalýþmasý yeterli
+
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +41,6 @@ namespace MVCDockerVolume
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
